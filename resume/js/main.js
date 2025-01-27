68e0a5c9d3381ecdaed8facda2ad90f3396a451d
fangxi <1096015776@qq.com>
2025-01-27 23:17:05 +0800

    shot

--- resume/js/main.js
+++ resume/js/main.js
@@ -1,3 +1,6 @@
+setTimeout(() => {
+  $('#siteWelcome').hide()
+}, 2000);
 document.onscroll = (e) => {
   if (window.scrollY > 0) {
     $(".site-header-wrapper").addClass("site-header-sticky")
@@ -5,3 +8,27 @@ document.onscroll = (e) => {
     $(".site-header-wrapper").removeClass("site-header-sticky")
   }
 }
+$('#categoryBarInner').css({
+  width: $('#all').width()
+})
+
+$("#all").on('click', () => {
+  $('#categoryBarInner').css({
+    width: $('#all').width(),
+    marginLeft: '0px'
+  })
+});
+
+$("#framework").on('click', () => {
+  $('#categoryBarInner').css({
+    width: $('#framework').width(),
+    marginLeft: $('#framework').offset().left - $("#categoryBar").offset().left
+  })
+});
+
+$("#originJs").on('click', () => {
+  $('#categoryBarInner').css({
+    width: $('#originJs').width(),
+    marginLeft: $('#originJs').offset().left - $("#categoryBar").offset().left
+  })
+});
