198e52edbe7db31feeb7f59aa4c8cd1827f71036
fangxi <1096015776@qq.com>
2025-03-04 19:54:00 +0800

    vue3项目更新

--- vue3-ts/src/App.vue
+++ vue3-ts/src/App.vue
@@ -2,17 +2,12 @@
 import Avatar from '@/components/Avatar.vue'
 import female from '@/assets/images/face-female-3.jpg'
 import Badge from '@/components/Badge.vue'
+import NavBar from './components/NavBar.vue'
 import { Status } from "@/enum/index"
 import smileIcon from "@/assets/icons/smile.svg"
 </script>
 
 <template>
-  <Avatar :src="female" :status="Status.Online"></Avatar>
-  <Badge :count="12">
-    <template #icon>
-      <icon :data="smileIcon" :width="24" :height="24" color="#4F9DDE"></icon>
-    </template>
-  </Badge>
-  <Badge :count="12" :show-zero="false"></Badge>
+  <NavBar></NavBar>
 
 </template>
---
+++ vue3-ts/src/components/NavBar.vue
@@ -0,0 +1,91 @@
+<script setup lang="ts">
+import { FontAwesomeIcon } from '@fortawesome/vue-fontawesome'
+import { faCog, faCommentDots, faEllipsisH, faFolder, faStickyNote, faUser } from '@fortawesome/free-solid-svg-icons'
+import Avatar from './Avatar.vue';
+import faceIcon from "@/assets/images/face-female-1.jpg"
+</script>
+<template>
+  <nav class="navbar-container">
+    <Avatar :src="faceIcon"></Avatar>
+    <div class="menus">
+      <div class="menu-item">
+        <a href="#" class="active">
+          <FontAwesomeIcon :icon="faCommentDots" style="color:white;" size="xl"></FontAwesomeIcon>
+        </a>
+      </div>
+      <div class="menu-item">
+        <a href="#">
+          <FontAwesomeIcon :icon="faUser" style="color:white" size="xl"></FontAwesomeIcon>
+        </a>
+      </div>
+      <div class="menu-item">
+        <a href="#">
+          <FontAwesomeIcon :icon="faFolder" style="color:white;" size="xl"></FontAwesomeIcon>
+        </a>
+      </div>
+      <div class="menu-item">
+        <a href="#">
+          <FontAwesomeIcon :icon="faStickyNote" style="color:white;" size="xl"></FontAwesomeIcon>
+        </a>
+      </div>
+      <div class="menu-item">
+        <a href="#">
+          <FontAwesomeIcon :icon="faEllipsisH" style="color:white;" size="xl"></FontAwesomeIcon>
+        </a>
+      </div>
+      <div class="menu-item">
+        <a href="#">
+          <FontAwesomeIcon :icon="faCog" style="color:white;" size="xl"></FontAwesomeIcon>
+        </a>
+      </div>
+    </div>
+
+  </nav>
+</template>
+<style lang="scss" scoped>
+.navbar-container {
+  padding: 30px 0;
+  background-color: $darkPurple;
+  height: 100vh;
+  width: 100px;
+  display: grid;
+  grid-template-rows: 1fr 4fr;
+
+  .avatar {
+    justify-self: center;
+  }
+
+  .menus {
+    display: grid;
+    grid-template-columns: repeat(5, minmax(auto, 88px), 1fr);
+
+    .menu-item {
+      &>a {
+        width: 100%;
+        height: 74px;
+        display: flex;
+        font-size: 16px;
+        align-items: center;
+        justify-content: center;
+
+        &.active {
+          @include activeBar
+        }
+      }
+
+
+      display:flex;
+      color: white;
+      font-size: 24px;
+
+      &:nth-last-child(1) {
+        align-self: end;
+      }
+
+
+    }
+
+  }
+
+}
+</style>
